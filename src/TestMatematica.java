c808e3152f0ec9fa803a71571c8e024754e2bfe1
Carlos Eduardo Santana Azuma <carlos.azuma@gmail.com>
2017-09-10 18:58:06 -0300

    Alteracao de Exemplo

---
+++ src/TestMatematica.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestMatematica {
+	
+	@Test
+	public void testSoma() {
+			assertEquals("10 + 1 = 11",11, Matematica.soma(10,1));
+	}
+}
